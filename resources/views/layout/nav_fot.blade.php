5e088772a1764f91d3f62a1809013cce0e2de18d
Okolo Emeka <okoloemeka37@gmail.com>
2024-06-29 22:59:25 +0100

    aye

--- resources/views/layout/nav_fot.blade.php
+++ resources/views/layout/nav_fot.blade.php
@@ -6,21 +6,21 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" href="{{asset('mine/lol.png')}}" type="image/x-icon">
 
-    <title>Document</title>
+    <title>ZylerBlog.com</title>
     <link rel="stylesheet" href="{{asset('css/app.css')}}">
         <link rel="stylesheet" href="{{asset('css/index.css')}}">
         <link rel="stylesheet" href="{{asset('css/foot.css')}}">
         <script src="{{asset('ckeditor/ckeditor.js')}}"></script>
 
-        <script src="{{asset('script/jquery.js')}}" defer></script> 
-        <script src="{{asset('script/index.js')}}" defer></script> 
-    <script src="{{asset('script/more.js')}}" defer></script> 
+        <script src="{{asset('script/jquery.js')}}" defer></script>
+        <script src="{{asset('script/index.js')}}" defer></script>
+    <script src="{{asset('script/more.js')}}" defer></script>
 
 </head>
 <body>
   <div class="d-flex justify-content-between sticky-top bg-light shadow-lg pl-1 pl-2 ">
-   
-  <div class="mt-3"><a class="" href="{{route('blog_index')}}"><p>Zyler</p></a></div> 
+
+  <div class="mt-3"><a class="" href="{{route('blog_index')}}"><p>Zyler</p></a></div>
     <ul class=" nav mt-1 show " id="nav">
         <li class="nav-item "><a href="{{route('about_show')}}" class="nav-link">About</a></li>
         <li class="nav-item text-primary"><a href="{{route('contact_show')}}" class="nav-link">Contact</a></li>
@@ -30,24 +30,24 @@
 
        @endguest
        @auth
-      
+
         <li class="nav-item text-primary"><a href="{{route('dashboard')}}" class="nav-link">{{auth()->user()->name}}</a></li>
-          
+
        <li class="nav-item text-primary">
-          <form action="http://zylerblog.com.cleverapps.io/logout" method="post">
+          <form action="https://app-37c9113d-6af0-447c-b7e8-acf4bdb4aaa1.cleverapps.io/logout" method="post">
             <input type="hidden" name="_token" value="{{ csrf_token() }}">
           <button class=" my-btn  text-primary">Logout</button>
-          </form>    
-          </li>     
+          </form>
+          </li>
             @endauth
 
-           
+
       </ul>
   <div class="tri-line" id="toggle-nav">
               <div class="line"></div>
               <div class="line"></div>
               <div class="line"></div>
-                   
+
             </div>
     </div>
   @yield('content')
@@ -56,7 +56,7 @@
 
 
 
- 
+
 
 </body>
 </html>
--- resources/views/layout/pub_nav.blade.php
+++ resources/views/layout/pub_nav.blade.php
@@ -5,22 +5,22 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" href="{{asset('mine/lol.png')}}" type="image/x-icon">
-    <title>Document</title>
+    <title>Zylerblog.com</title>
     <link rel="stylesheet" href="{{asset('css/app.css')}}">
         <link rel="stylesheet" href="{{asset('css/index.css')}}">
         <link rel="stylesheet" href="{{asset('css/foot.css')}}">
-       
 
-        <script src="{{asset('script/jquery.js')}}" defer></script> 
+
+        <script src="{{asset('script/jquery.js')}}" defer></script>
         <script src="{{asset('script/index.js')}}" defer></script>
         <script src="{{asset('script/simp.js')}}" defer></script>
-    <script src="{{asset('script/more.js')}}" defer></script> 
+    <script src="{{asset('script/more.js')}}" defer></script>
     <script src="https://kit.fontawesome.com/d335dcf51b.js" crossorigin="anonymous"></script>
 </head>
 <body>
   <div class="d-flex justify-content-between sticky-top bg-light shadow-lg pl-1 pl-2 ">
-   
-  <div class="mt-3"><a class="" href="{{route('blog_index')}}"><p>Zyler</p></a></div> 
+
+  <div class="mt-3"><a class="" href="{{route('blog_index')}}"><p>Zyler</p></a></div>
     <ul class=" nav mt-1 show " id="nav">
         <li class="nav-item "><a href="{{route('about_show')}}" class="nav-link">About</a></li>
         <li class="nav-item text-primary"><a href="{{route('contact_show')}}" class="nav-link">Contact</a></li>
@@ -31,15 +31,15 @@
         <li class="nav-item text-primary"><a href="{{route('dashboard')}}" class="nav-link">{{auth()->user()->name}}</a></li>
        @endif
         @endauth
-         
-        
-   
+
+
+
    </ul>
   <div class="tri-line" id="toggle-nav">
               <div class="line"></div>
               <div class="line"></div>
               <div class="line"></div>
-                   
+
             </div>
     </div>
 
@@ -47,7 +47,7 @@
 @yield('content')
 
 
-  
+
 
   <!-- Site footer -->
   <footer class="site-footer">
@@ -74,7 +74,7 @@
           <ul class="footer-links">
             <li><a href="{{route('about_show')}}">About Us</a></li>
             <li><a href="https://okoloemeka37.github.io/Portfolio/">Contact Us</a></li>
-           
+
           </ul>
         </div>
       </div>
@@ -93,7 +93,7 @@
             <li><a class="facebook" href="https://web.facebook.com/emeka.okolo.5201"><i class="fa fa-facebook"></i></a></li>
             <li><a class="twitter" href="https://x.com/zylercodes?t=wb2Ah1UrbAUgYASqkIwZsQ&s=09&mx=2"><i class="fa-brands fa-x-twitter"></i></a></li>
             <li><a class="dribbble" href="https://github.com/okoloemeka37"><i class="fa-brands fa-github"></i></a></li>
-            <li><a class="linkedin" href="https://www.linkedin.com/in/okolo-emeka-402295254/"><i class="fa fa-linkedin"></i></a></li>   
+            <li><a class="linkedin" href="https://www.linkedin.com/in/okolo-emeka-402295254/"><i class="fa fa-linkedin"></i></a></li>
           </ul>
         </div>
       </div>
