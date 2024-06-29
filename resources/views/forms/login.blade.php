c9aa59638acb328faa7014fb6e9884498b70a894
Okolo Emeka <okoloemeka37@gmail.com>
2024-06-29 16:38:20 +0100

    Update login.blade.php

--- resources/views/forms/login.blade.php
+++ resources/views/forms/login.blade.php
@@ -4,7 +4,7 @@
 
 
 
-<form action="{{url('https://zylerblog.com.cleverapps.io/loginPost')}}" method="post" class="centered login mt-5">
+<form action="{{url('http://zylerblog.com.cleverapps.io/loginPost')}}" method="post" class="centered login mt-5">
 
     <input type="hidden" name="_token" value="{{ csrf_token() }}">
 
